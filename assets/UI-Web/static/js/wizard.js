7057c9c3b311f109d839b34f11d1bc172ed751b6
Asfia <31125445+asfiasiddiqui@users.noreply.github.com>
2017-12-14 13:40:57 +0530

    Add css files

---
+++ assets/UI-Web/static/js/wizard.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+document.getElementById("navigate_to_ccds").addEventListener("click", function(event){
+	event.preventDefault();
+	window.location.href = 'ccds.html'
+	});    
+
+});
+
