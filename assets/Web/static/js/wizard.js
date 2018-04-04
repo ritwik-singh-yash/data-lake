06f69149f3716b69074cd24d7a28834c325eaae0
EC2 Default User <ec2-user@ip-172-31-24-30.us-west-2.compute.internal>
2018-04-04 13:43:49 +0000

    cleaning the repo

--- assets/Web/static/js/wizard.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-document.getElementById("navigate_to_ccds").addEventListener("click", function(event){
-	event.preventDefault();
-	window.location.href = 'ccds.html'
-	});    
-
-});
-
