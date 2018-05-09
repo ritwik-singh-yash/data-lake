a428417a5638b8d1054a9fa344d18d18f7df02af
EC2 Default User <ec2-user@ip-172-31-24-30.us-west-2.compute.internal>
2018-05-09 13:28:57 +0000

    Final Templates

---
+++ templates/Datalake-with-Demo/copydata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+sudo yum install wget -y;
+mkdir datalake-data;
+cd datalake-data;
+wget https://$2.s3.amazonaws.com/$3/customers/customerData.csv;
+wget https://$2.s3.amazonaws.com/$3/customers/customer_updated_refrence.csv;
+wget https://$2.s3.amazonaws.com/$3/demographics/demographics_data_20170520.csv;
+wget https://$2.s3.amazonaws.com/$3/orders/order_data.csv;
+wget https://$2.s3.amazonaws.com/$3/products/products.csv2013;
+wget https://$2.s3.amazonaws.com/$3/products/products.csv2014;
+wget https://$2.s3.amazonaws.com/$3/products/products.csv2015;
+wget https://$2.s3.amazonaws.com/$3/products/products.csv2016;
+aws s3 cp demographics_data_20170520.csv s3://$1/demographics/;
+aws s3 cp customerData.csv s3://$1/customers/;
+aws s3 cp customer_updated_refrence.csv s3://$1/customers/;
+aws s3 cp order_data.csv s3://$1/orders/;
+aws s3 cp products.csv2013 s3://$1/products/;
+aws s3 cp products.csv2014 s3://$1/products/;
+aws s3 cp products.csv2015 s3://$1/products/;
+aws s3 cp products.csv2016 s3://$1/products/;
+
+
---
+++ templates/Datalake-with-Demo/websitedatacopy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+mkdir websiteData;
+cd websiteData;
+wget https://$1.s3.amazonaws.com/$2/V2-WEB.zip
+unzip V2-WEB.zip
+rm -rf V2-WEB.zip
+aws s3 sync /websiteData s3://$3 --acl public-read
+
+
